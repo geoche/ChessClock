1e15115dbfd6baec9b497c4d2d9c04f21d3d9390
George Cheban <4georgecheban@gmail.com>
2019-01-09 11:11:14 +0100

    0. Added PAUSE/CONTINUE button 1. Added visual limiter for each match.

--- Chess_clock/Form1.Designer.cs
+++ Chess_clock/Form1.Designer.cs
@@ -44,13 +44,22 @@
             this.label10 = new System.Windows.Forms.Label();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(42, 90);
+            this.label1.Location = new System.Drawing.Point(7, 95);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 25);
             this.label1.TabIndex = 0;
@@ -62,7 +71,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(108, 90);
+            this.label2.Location = new System.Drawing.Point(73, 95);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 25);
             this.label2.TabIndex = 1;
@@ -71,21 +80,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(47, 119);
+            this.button1.Location = new System.Drawing.Point(12, 124);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(163, 35);
             this.button1.TabIndex = 2;
-            this.button1.Text = "PLAYER_I";
+            this.button1.Text = "PLAYER I";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(398, 119);
+            this.button2.Location = new System.Drawing.Point(363, 124);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(163, 35);
             this.button2.TabIndex = 3;
-            this.button2.Text = "PLAYER_II";
+            this.button2.Text = "PLAYER II";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -93,7 +102,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(84, 90);
+            this.label3.Location = new System.Drawing.Point(49, 95);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(18, 26);
             this.label3.TabIndex = 7;
@@ -104,7 +113,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(393, 90);
+            this.label4.Location = new System.Drawing.Point(358, 95);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 25);
             this.label4.TabIndex = 8;
@@ -115,7 +124,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(435, 90);
+            this.label5.Location = new System.Drawing.Point(400, 95);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(18, 26);
             this.label5.TabIndex = 9;
@@ -126,7 +135,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(459, 90);
+            this.label6.Location = new System.Drawing.Point(424, 95);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(34, 25);
             this.label6.TabIndex = 10;
@@ -142,7 +151,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(174, 90);
+            this.label7.Location = new System.Drawing.Point(139, 95);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(34, 25);
             this.label7.TabIndex = 11;
@@ -153,7 +162,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(150, 90);
+            this.label8.Location = new System.Drawing.Point(115, 95);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(18, 26);
             this.label8.TabIndex = 12;
@@ -163,7 +172,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label9.Location = new System.Drawing.Point(525, 90);
+            this.label9.Location = new System.Drawing.Point(490, 95);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(34, 25);
             this.label9.TabIndex = 13;
@@ -174,7 +183,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label10.Location = new System.Drawing.Point(501, 90);
+            this.label10.Location = new System.Drawing.Point(466, 95);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(18, 26);
             this.label10.TabIndex = 14;
@@ -188,7 +197,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(217, 119);
+            this.button3.Location = new System.Drawing.Point(182, 124);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(175, 35);
             this.button3.TabIndex = 15;
@@ -196,11 +205,84 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(182, 92);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "PAUSE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(182, 22);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 22);
+            this.numericUpDown1.TabIndex = 17;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(282, 22);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(75, 22);
+            this.numericUpDown2.TabIndex = 18;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(231, 72);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(24, 17);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "00";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(261, 72);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(12, 17);
+            this.label12.TabIndex = 20;
+            this.label12.Text = ":";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(279, 72);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(24, 17);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "00";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(183, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(175, 23);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "LIMIT";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(615, 201);
+            this.ClientSize = new System.Drawing.Size(541, 174);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -217,6 +299,8 @@
             this.Name = "Form1";
             this.Text = "Chess Clock";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -239,6 +323,13 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Chess_clock/Form1.cs
+++ Chess_clock/Form1.cs
@@ -112,6 +112,84 @@ namespace Chess_clock
 
         }
 
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = 9;
+            numericUpDown1.Minimum= 0;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown2.Maximum = 59;
+            numericUpDown2.Minimum = 0;
+            
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            label13.Text = "00";         
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown2.Value < 59)
+            {
+                label13.Text = numericUpDown2.Value.ToString();
+                if (numericUpDown2.Value < 10)
+                    label13.Text = "0" + numericUpDown2.Value.ToString();
+            }
+            else
+                label13.Text = "00";
+
+            if (numericUpDown1.Value < 10)
+                label11.Text = "0" + numericUpDown1.Value.ToString();
+            else
+                label11.Text = "00";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true && timer2.Enabled == false)
+            {
+                timer1.Enabled = false;
+                button4.Text = "CONTINUE";
+            }
+            else if (timer1.Enabled == false && timer2.Enabled == true)
+            {
+                timer2.Enabled = false;
+                button4.Text = "CONTINUE";
+            }
+
+            else if(s1 != 0 || m1 != 0 || h1 != 0 || s2 != 0 || m2 != 0 || h2 != 0)
+            {
+                if (button1.Enabled == false)
+                {
+                    timer2.Enabled = true;
+                    button4.Text = "PAUSE";
+                }
+                    
+                else if (button2.Enabled == false)
+                {
+
+                    timer1.Enabled = true;
+                    button4.Text = "PAUSE";
+                }
+
+
+
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
